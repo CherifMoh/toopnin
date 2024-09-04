05a248f2729d4b09a48fef397873562fa0fbdcb6
CherifMoh <mircho.dev@gmail.com>
2024-09-04 21:00:28 +0100

    fix

--- src/components/admin/pixel/Allpixels.jsx
+++ src/components/admin/pixel/Allpixels.jsx
@@ -1,9 +1,15 @@
 
 import axios from 'axios';
 import PixelScript from './PixelScript'
+import https from 'https';
 
 async function fetchPixels() {
-    const res = await axios.get('https://toopnin.com/api/pixel');
+    const agent = new https.Agent({  
+        rejectUnauthorized: false
+    });
+    const res = await axios.get('https://toopnin.com/api/pixel', {
+        httpsAgent: agent
+    });
     return res.data;
 }
 
