d70b23cfbedef51f9cb5a0d323c6c609934979a6
CherifMoh <mircho.dev@gmail.com>
2024-12-03 16:41:05 +0100

    fix

--- src/app/admin/orders/page.jsx
+++ src/app/admin/orders/page.jsx
@@ -270,19 +270,24 @@ function Orders() {
         const fetchAndUpdateOrders = async () => {
             try {
                 const dateFilter = await ZrfetchDate()
-                // console.log(dateFilter)
-                // if(!dateFilter) return
+                console.log(dateFilter)
+                if(!dateFilter) return
                 const relevantOrders = Orders.filter(
                     (order) =>
-                        order.deliveryAgent === 'ZR' && order.state === 'مؤكدة'
+                        order.deliveryAgent === 'ZR' 
+                        && order.state === 'مؤكدة'
+                        && order.tracking !== 'Retour de Dispatche'
+                        && order.tracking !== 'Livrée [ Recouvert ]'
+
+
                 );
     
                 if (relevantOrders.length === 0) return;
     
-                
+               
                 // Fetch tracking statuses for all relevant orders
-                // const trackingData = await fetchAllOrderStatuses(relevantOrders);
-                // console.log(trackingData)
+                const trackingData = await fetchAllOrderStatuses(relevantOrders);
+                console.log(trackingData)
     
                 // Update orders in the database
                 // await Promise.all(
@@ -298,7 +303,7 @@ function Orders() {
                 //     })
                 // );
     
-                // queryClient.invalidateQueries(['orders', dateFilter]);
+                queryClient.invalidateQueries(['orders', dateFilter]);
             } catch (err) {
                 setOrdersUpdted(false);
                 console.log(err);
