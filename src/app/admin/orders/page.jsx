8ca49affb9135b87f831cb9a2e09f7c7ce0906a4
CherifMoh <mircho.dev@gmail.com>
2024-12-22 17:07:40 +0100

    Prêt à expédier L

--- src/app/admin/orders/page.jsx
+++ src/app/admin/orders/page.jsx
@@ -928,7 +928,7 @@ function Orders() {
             }
         }
 
-        if(oldOrder.inDelivery !== true && editedOrder.inDelivery  === true){
+        if(oldOrder.inDelivery !== true && editedOrder.inDelivery  === true&& editedOrder.deliveryAgent !== 'Livreur'){
             newOrder = {
                 ...newOrder,
                 tracking : 'Prêt à expédier'
